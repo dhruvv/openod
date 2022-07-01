c7c0a886817a147736d11af6dfd6e9b1538a31d2
Dhruv Venkataraman <dhruv@dhruvv.tech>
2022-07-01 08:51:47 -0500

    Changed modal

--- frontend/src/components/LoadingModal.js
+++ frontend/src/components/LoadingModal.js
@@ -1,16 +1,28 @@
-import { Modal } from '@mui/material';
+import { Modal, Box} from '@mui/material';
 import LoadingIcons from 'react-loading-icons'
 
 export default function LoadingModal(props) {
-
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
 
     return(
-        <Modal
-        open={props.modalOpen}
-        onClose={props.handleModalClose}
-        >
-        <LoadingIcons.Bars />
-        </Modal>
+        <Box sx={style}>
+          <Modal
+                open={props.modalOpen}
+                onClose={props.handleModalClose}
+            >
+                <LoadingIcons.Bars />
+            </Modal>
+        </Box>
     )
     
 }
